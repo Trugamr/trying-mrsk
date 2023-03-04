1174578ea9ef7194010635d787a9ab61d1d60bfa
Trugamr <trugamr@live.in>
2023-03-05 01:32:37 +0530

    add mrsk healthcheck route

--- app/index.js
+++ app/index.js
@@ -9,6 +9,10 @@ app.get('/', async (request, reply) => {
   })
 })
 
+app.get('/health', async (request, reply) => {
+  await reply.send("OK")
+})
+
 const port = process.env.PORT
 app.listen({ port, host: '0.0.0.0' })
 
